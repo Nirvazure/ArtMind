01f1d3247957db7072d8ef05c9d699818912965d
yckj3529 <lifurong1@cloudwalk.com>
2024-04-26 00:45:47 +0800

    上传图片到OSS

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,18 @@
-<template>Artmind</template>
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="1" v-for="(v, i) in painters" :key="i">
+        <v-avatar size="100">
+          <v-img
+            :src="`http://yqymons-bank.oss-cn-beijing.aliyuncs.com/artmind/images/painters/${v.name}.jpg`"
+          ></v-img>
+        </v-avatar>
+        <h1>{{ v.name }}</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
 
 <script lang="ts" setup>
-//
+import { painters } from "../api/painters";
 </script>
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 export function getImage(dir: string, name: string, picType = 'jpg' as string) {
-    const url = decodeURI(`/assets/${dir}/${name}.${picType}`)
-    return new URL(url, import.meta.url).href
+    // const url = decodeURI(`/assets/${dir}/${name}.${picType}`)
+    // return new URL(url, import.meta.url).href
+    return `http://yqymons-bank.oss-cn-beijing.aliyuncs.com/artmind/images/${dir}/${name}.${picType}`
 }
 

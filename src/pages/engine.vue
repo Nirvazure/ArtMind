59a0e0e07f9cb6b7ec4bbf464ed4d3fec0ce25ab
yckj3529 <lifurong1@cloudwalk.com>
2024-10-17 21:31:41 +0800

    页面优化

--- src/pages/engine.vue
+++ src/pages/engine.vue
@@ -2,12 +2,18 @@
   <v-container>
     <h1 class="text-center">ArtMind</h1>
     <h3 class="text-center font-weight-thin">Engines</h3>
+
     <v-divider class="my-8" />
     <v-row justify="center">
       <v-sheet width="1000" class="mt-8">
         <v-row>
           <v-col v-for="(v, i) in models" :key="i" cols="3">
-            <v-card :disabled="!v.isSwitch" shaped>
+            <v-card
+              :disabled="!v.isSwitch"
+              shaped
+              :color="switches.includes(v.name) ? v.color : 'white'"
+              class="text-white"
+            >
               <h3 class="text-center mt-3">{{ v.name }}</h3>
               <h5 class="text-center font-weight-thin">Engines</h5>
               <v-divider class="ma-5" />
@@ -15,7 +21,8 @@
               <v-card-text>
                 <v-row justify="center">
                   <v-switch
-                    v-model="v.isSwitch"
+                    v-model="switches"
+                    :value="v.name"
                     hide-details
                     inset
                     :color="v.color"
@@ -25,21 +32,21 @@
               <v-card-text>
                 <v-row justify="center">
                   <v-progress-circular
-                    model-value="20"
+                    :model-value="v.preciseNum"
                     width="10"
                     size="130"
-                    :color="v.color"
+                    :color="switches.includes(v.name) ? 'white' : v.color"
                   >
-                    <p class="text-h6">20%</p>
+                    <p class="text-h6">{{ v.preciseNum }}%</p>
                   </v-progress-circular>
                 </v-row>
               </v-card-text>
 
               <v-divider class="ma-8" />
               <div class="ma-3">
-                <h4 class="text-center text-grey font-weight-thin">数据集数量</h4>
+                <h4 class="text-center text-white font-weight-thin">数据集数量</h4>
                 <count-up
-                  class="text-center font-weight-black text-grey"
+                  class="text-center font-weight-black text-white"
                   :end-val="2000"
                 ></count-up>
               </div>
@@ -52,39 +59,38 @@
 </template>
 
 <script lang="ts">
+import { color } from "chart.js/helpers";
 import CountUp from "vue-countup-v3";
 export default {
   data: () => ({
+    switches: ["西方油画", "中国水墨", "日本浮世绘"] as Array<string>,
     models: [
       {
         name: "西方油画",
         description: "用于西方流派分析",
-        // avatar: require("@/assets/west.jpg"),
         status: "已部署",
         isSwitch: true,
-        color: "cyan",
+        color: "red",
+        preciseNum: 76,
       },
       {
-        name: "中国山水",
+        name: "中国水墨",
         description: "用于中国风山水画分析",
-        // avatar: require("@/assets/china.jpg"),
         status: "已部署",
         isSwitch: true,
         color: "teal",
+        preciseNum: 96,
       },
       {
         name: "日本浮世绘",
         description: "用于日式浮世绘风格分析",
-        // avatar: require("@/assets/japan.jpg"),
-        status: "已部署",
         isSwitch: true,
         color: "orange",
+        preciseNum: 46,
       },
       {
-        name: "中国山水",
+        name: "波普艺术",
         description: "用于中国风山水画分析",
-        // avatar: require("@/assets/china.jpg"),
-        status: "待应用",
         isSwitch: false,
         // color: "teal"
       },
@@ -92,16 +98,11 @@ export default {
     model: true,
   }),
   components: {
-    // eslint-disable-next-line vue/no-unused-components
     CountUp,
   },
-  created() {
-    this.toggles = this.models.map((v) => v.isSwitch);
-  },
   methods: {
-    toggleEngine(engineName: any) {
-      const cur = this.models.findIndex((v: { name: string }) => v.name === engineName);
-      this.models[cur].switch = !this.models[cur].switch;
+    toggleEngine(engineId: number) {
+      this.models[engineId].isSwitch = !this.models[engineId].isSwitch;
     },
   },
 };
--- src/pages/painter.vue
+++ src/pages/painter.vue
@@ -1,35 +1,47 @@
 <template>
   <v-container>
     <v-container>
-      <v-row>
-        <v-col v-for="(painter, i) in painters" :key="i" cols="12" md="2">
-          <v-card flat class="basil">
-            <v-card-text class="text-center">
-              <v-avatar size="100">
-                <v-img
-                  :src="`http://yqymons-bank.oss-cn-beijing.aliyuncs.com/artmind/images/painters/${painter.name}.jpg`"
-                ></v-img>
-              </v-avatar>
-              <p>{{ painter.name }}</p>
-              <v-chip small dark color="blue">文艺复兴</v-chip>
-            </v-card-text>
-            <v-card-actions class="white justify-center">
-              <v-btn
-                v-for="(social, i) in socials"
-                :key="i"
-                :color="social.color"
-                class="white--text"
-                fab
-                icon
-                small
-              >
-                <v-icon>{{ social.icon }}</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-        <v-divider vertical></v-divider>
-      </v-row>
+      <v-data-iterator :items="painters" :page="page" :items-per-page="18">
+        <template v-slot:default="{ items }">
+          <v-row>
+            <v-col v-for="(painter, i) in items" :key="i" cols="12" md="2">
+              <v-card flat class="basil">
+                <v-card-text class="text-center">
+                  <v-avatar size="100">
+                    <v-img
+                      :src="`http://yqymons-bank.oss-cn-beijing.aliyuncs.com/artmind/images/painters/${painter.raw.name}.jpg`"
+                    ></v-img>
+                  </v-avatar>
+                  <p>{{ painter.raw.name }}</p>
+                  <v-chip small dark color="blue">文艺复兴</v-chip>
+                </v-card-text>
+                <v-card-actions class="white justify-center">
+                  <v-btn
+                    v-for="(social, i) in socials"
+                    :key="i"
+                    :color="social.color"
+                    class="white--text"
+                    fab
+                    icon
+                    small
+                  >
+                    <v-icon>{{ social.icon }}</v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+            <v-divider vertical></v-divider>
+          </v-row>
+        </template>
+      </v-data-iterator>
+
+      <v-pagination
+        class="mt-5"
+        color="teal"
+        v-model="page"
+        :length="pageLength"
+        rounded="circle"
+      ></v-pagination>
     </v-container>
   </v-container>
 </template>
@@ -38,6 +50,7 @@
 import { painters } from "../api/painters";
 export default {
   data: () => ({
+    page: 1,
     socials: [
       {
         icon: "mdi-facebook",
@@ -54,6 +67,11 @@ export default {
     ],
     painters: painters,
   }),
+  computed: {
+    pageLength() {
+      return Math.floor(this.painters.length / 12);
+    },
+  },
 };
 </script>
 
